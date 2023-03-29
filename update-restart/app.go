da32783c4cbc8901ba63a00a1928088bac9df220
wwestgarth <william@vega.xyz>
2023-03-29 12:56:59 +0100

    init app and copy from riftshare

---
+++ update-restart/app.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/blang/semver"
+	"github.com/rhysd/go-github-selfupdate/selfupdate"
+)
+
+const Version = "0.1.0"
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called at application startup
+func (b *App) startup(ctx context.Context) {
+	// Perform your setup here
+	b.ctx = ctx
+}
+
+// domReady is called after the front-end dom has been loaded
+func (b *App) domReady(ctx context.Context) {
+	// Add your action here
+}
+
+// shutdown is called at application termination
+func (b *App) shutdown(ctx context.Context) {
+	// Perform your teardown here
+}
+
+// Greet returns a greeting for the given name
+func (b *App) Greet(name string) string {
+	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func SeltUpdate() bool {
+	v := semver.MustParse(Version)
+	latest, err := selfupdate.UpdateSelf(v, "achhabra2/riftshare")
+	if err != nil {
+		log.Println("Binary update failed:", err)
+		return false
+	}
+	if latest.Version.Equals(v) {
+		// latest version is the same as current version. It means current binary is up to date.
+		log.Println("Current binary is the latest version", Version)
+		return true
+	} else {
+		log.Println("Successfully updated to version", latest.Version)
+		log.Println("Release note:\n", latest.ReleaseNotes)
+		return true
+	}
+}
+
+func SelfUpdateMac() bool {
+	latest, found, _ := selfupdate.DetectLatest("achhabra2/riftshare")
+	if found {
+		homeDir, _ := os.UserHomeDir()
+		downloadPath := filepath.Join(homeDir, "Downloads", "RiftShare.zip")
+		err := exec.Command("curl", "-L", latest.AssetURL, "-o", downloadPath).Run()
+		if err != nil {
+			log.Println("curl error:", err)
+			return false
+		}
+		var appPath string
+		cmdPath, err := os.Executable()
+		appPath = strings.TrimSuffix(cmdPath, "RiftShare.app/Contents/MacOS/RiftShare")
+		if err != nil {
+			appPath = "/Applications/"
+		}
+		err = exec.Command("ditto", "-xk", downloadPath, appPath).Run()
+		if err != nil {
+			log.Println("ditto error:", err)
+			return false
+		}
+		err = exec.Command("rm", downloadPath).Run()
+		if err != nil {
+			log.Println("removing error:", err)
+			return false
+		}
+		return true
+	} else {
+		return false
+	}
+}
+
+func CheckForUpdate() (bool, string) {
+	/*
+			latest, found, err := selfupdate.DetectLatest("achhabra2/riftshare")
+			if err != nil {
+				log.Println("Error occurred while detecting version:", err)
+				return false, ""
+			}
+
+			v := semver.MustParse(Version)
+			if !found || latest.Version.LTE(v) {
+				log.Println("Current version is the latest")
+				return false, ""
+			}
+		return true, latest.Version.String()
+	*/
+
+	// pretend we always have a latest
+	return true, "0.2.0"
+}
---
+++ update-restart/go.mod
@@ -0,0 +1,54 @@
+module changeme
+
+go 1.17
+
+require (
+	github.com/blang/semver v3.5.1+incompatible
+	github.com/wailsapp/wails/v2 v2.0.0-beta.32
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-github/v30 v30.1.0 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/inconshreveable/go-update v0.0.0-20160112193335-8152e7eb6ccf // indirect
+	github.com/tcnksm/go-gitconfig v0.1.2 // indirect
+	github.com/ulikunitz/xz v0.5.9 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/oauth2 v0.0.0-20181106182150-f42d05182288 // indirect
+	google.golang.org/appengine v1.3.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fasthttp/websocket v0.0.0-20200320073529-1554a54587ab // indirect
+	github.com/gabriel-vasile/mimetype v1.3.1 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/gofiber/fiber/v2 v2.17.0 // indirect
+	github.com/gofiber/websocket/v2 v2.0.8 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
+	github.com/klauspost/compress v1.12.2 // indirect
+	github.com/leaanthony/go-ansi-parser v1.0.1 // indirect
+	github.com/leaanthony/go-common-file-dialog v1.0.3 // indirect
+	github.com/leaanthony/go-webview2 v1.0.3-0.20220125181555-00f20a61c17d // indirect
+	github.com/leaanthony/gosod v1.0.3 // indirect
+	github.com/leaanthony/slicer v1.5.0 // indirect
+	github.com/leaanthony/typescriptify-golang-structs v0.1.7 // indirect
+	github.com/leaanthony/winc v0.0.0-20220208061147-37b059b9dc3b // indirect
+	github.com/pkg/browser v0.0.0-20210706143420-7d21f8c997e2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rhysd/go-github-selfupdate v1.2.3
+	github.com/savsgio/gotils v0.0.0-20200117113501-90175b0fbe3f // indirect
+	github.com/tkrajina/go-reflector v0.5.5 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.28.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+)
+
+// replace github.com/wailsapp/wails/v2 v2.0.0-beta.32 => /Users/wwestgarth/go2/pkg/mod/github.com/wailsapp/wails/v2@v2.0.0-beta.32
